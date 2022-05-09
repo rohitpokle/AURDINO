95f452eae3552b4a2d8092fdf13ac90b123044b4
rohitpokle <101121969+rohitpokle@users.noreply.github.com>
2022-05-09 18:10:07 +0530

    Add files via upload
    THIS PROJECT IS ONLY TO CHECK WHETHER MOTOR DRIVE , AURDINO  MEGA 2560 AND MOTOR IS WORKING PROPERLY IN DIFFERENT SPEED

---
+++ sketch_may09a.ino
@@ -0,0 +1,95 @@
+int en1 = 5;
+int in1 = 6;
+int in2 = 7;
+int in3 = 8;
+int in4 = 9;
+int en2 = 10;
+
+void setup() {
+
+  pinMode(en1, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  //MOTOR_A CLOCKWISE MAX SPEED
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(en1, 255);
+
+  //MOTOR_B CLOCKWISE MAX SPEED
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(en2, 255);
+  delay(2000);
+
+  //STOP
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  delay(2000);
+
+  //MOTOR_A COUNTERCLOCKWISE MAX SPEED
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  analogWrite(en1, 255);
+
+  //MOTOR_B COUNTERCLOCKWISE MAX SPEED
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  analogWrite(en2, 255);
+
+  delay(2000);
+
+  //STOP
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  delay(2000);
+
+
+  //MOTOR_A CLOCKWISE HALF SPEED
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(en1, 127);
+
+  //MOTOR_B CLOCKWISE HALF SPEED
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(en2, 127);
+  delay(2000);
+
+  //STOP
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  delay(2000);
+
+  //MOTOR_A COUNTERCLOCKWISE MAX SPEED
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  analogWrite(en1, 127);
+
+  //MOTOR_B COUNTERCLOCKWISE MAX SPEED
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  analogWrite(en2, 127);
+  delay(2000);
+
+  //STOP
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  delay(2000); 
+}
+
+void loop() {
+
+
+}
